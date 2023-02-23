ef459dc3e90e3508e39e524bf1d3ed8452c72b7f
Alexander Musonda <musondaalexander97@gmail.com>
2023-02-23 18:54:07 +0200

    updated the sollution to the twoSum  probelm

--- two_sum.py
+++ two_sum.py
@@ -2,12 +2,21 @@
 # The two numbers are added and the sum is compared to the target
 
 class Solution:
-   def twoSum(self, nums: list[int], target: int) -> list[int]:
-       seen = {}
-       for i, value in enumerate(nums): #1
-           remaining = target - nums[i] #2
-           
-           if remaining in seen: #3
-               return [i, seen[remaining]]  #4
-           else:
-               seen[value] = i  #5
+
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen = {}
+        listNum = []
+        for i, value in enumerate(nums):  # 1
+            remaining = target - nums[i]  # 2
+            listNum.append(value)
+
+            if remaining in seen:  # 3
+                return [i, seen[remaining]]  # 4
+            else:
+                seen[value] = i  # 5
+
+
+# Creating an objec of the class solution and calling the twoSum method
+so = Solution()
+print(so.twoSum([2, 7, 11, 15], 9))
+
