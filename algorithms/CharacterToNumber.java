e162c26758234309288a8d3c05eb6d580ffdc7b3
Alexander Musonda <musondaalexander97@gmail.com>
2023-03-16 00:47:39 +0200

    Character to Integer

---
+++ algorithms/CharacterToNumber.java
@@ -0,0 +1,24 @@
+package algorithms;
+
+public class CharacterToNumber {
+    
+
+    public static int parseInt(String text){
+        
+        int number = 0;
+        int powerIncreament =  0;
+
+        for (int i = text.length()- 1; i >=0; i--){
+            char c = text.charAt(i);
+            number += Math.pow(26, powerIncreament)* ((int)c - (int)'A' + 1);
+            powerIncreament++;
+        }
+        return number;
+    }
+        
+public static void main(String[] args) {
+
+    System.out.println(CharacterToNumber.parseInt("Z"));
+}
+
+}
